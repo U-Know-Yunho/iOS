62675d22d07bfbb31bea9308218bc929d479f8d6
Wi <dnltjdghks1@gmail.com>
2018-12-12 11:42:03 +0900

    test

--- CGVProject/CGVProject/AppDelegate.swift
+++ CGVProject/CGVProject/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // MARK: - App Life Cycle
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        //
         initializeApp()
         return true
     }
