85d534fe482132f4aa7662edf1b1c613596f3546
Maru <gorgeouskyu@gmail.com>
2018-12-19 01:29:25 +0900

    fixed distributionImageView

--- CGVProject/CGVProject/Kyu/MovieInfoTable/MovieCell/MovieInfoTableViewCell.swift
+++ CGVProject/CGVProject/Kyu/MovieInfoTable/MovieCell/MovieInfoTableViewCell.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import UserNotifications
 
 class MovieInfoTableViewCell: UITableViewCell {
     var moviePk: Int?
     var moviePhoto: [String] = []
+    var distributionCompany = ["20th_Century_Fox_logo", "20th_Century_Fox_logo", "columbiapictures", "newlinecinema", "paramount", "waltdisney", "showbox"]
     var stillcutURL: [MovieDetail.Stillcut]? {
         didSet {
             self.moviePhotoCollectView.reloadData()
@@ -26,10 +28,28 @@ class MovieInfoTableViewCell: UITableViewCell {
     @IBOutlet weak var customMovieNewsButton: UIButton!
     @IBOutlet weak var customMovieInfoButton: UIButton!
     @IBOutlet weak var likeImageView: UIImageView!
+    @IBOutlet weak var distributionImage: UIImageView!
     
-    @IBAction func shareButton(_ sender: Any) {
-        
-    }
+//    @IBAction func alarmButton(_ sender: Any) {
+//        UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.sound,.badge], completionHandler: {didAllow,Error in })
+//
+//        let content = UNMutableNotificationContent()
+//        content.title = "title: 타이틀"
+//        content.subtitle = "subtitle: 서브타이틀"
+//        content.body = "body: 바디"
+//        content.badge = 1
+//        content.sound = UNNotificationSound.default
+//
+//        let date = Date(timeIntervalSinceNow: 2)
+//        var dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date)
+//        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+//
+//        //Adding Request
+//        // MARK: - identifier가 다 달라야만 Notification Grouping이 된다
+//        let request = UNNotificationRequest(identifier: "\(index)timerdone", content: content, trigger: trigger)
+//
+//        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+//    }
     
     @IBAction func bookButton(_ sender: Any) {
         MainViewController.showCurrentMobvieBookPage(moviePk: moviePk ?? 0)
@@ -57,6 +77,9 @@ class MovieInfoTableViewCell: UITableViewCell {
         likeImageView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(likeImageTap)))
         movieTitleLabel.sizeToFit()
         movieTitleLabel.adjustsFontSizeToFitWidth = true
+        
+        distributionImage.image = UIImage(named: distributionCompany.randomElement() ?? "")
+//        UNUserNotificationCenter.current().delegate = self
     }
     
     @objc func likeImageTap(_ sender: UITapGestureRecognizer) {
@@ -108,6 +131,25 @@ extension MovieInfoTableViewCell: UICollectionViewDelegateFlowLayout {
         return collectionView.bounds.size
     }
 }
+
+//extension MovieInfoTableViewCell : UNUserNotificationCenterDelegate{
+//    //To display notifications when app is running  inforeground
+//
+//    //앱이 foreground에 있을 때. 즉 앱안에 있어도 push알림을 받게 해준다.
+//    //viewDidLoad()에 UNUserNotificationCenter.current().delegate = self를 추가해줄 것.
+//    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+//        completionHandler([.alert, .sound, .badge])
+//    }
+//
+//    func userNotificationCenter(_ center: UNUserNotificationCenter, openSettingsFor notification: UNNotification?) {
+//        let settingsViewController = UIViewController()
+//        settingsViewController.view.backgroundColor = .gray
+//        self.present(settingsViewController, animated: true, completion: nil)
+//
+//    }
+
+//}
+
 
 
 // 중간 view border line
