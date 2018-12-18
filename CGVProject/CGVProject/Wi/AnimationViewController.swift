d57011d6f0735de5c17c656704e1bad9786b63f6
Wi <dnltjdghks1@gmail.com>
2018-12-18 15:47:47 +0900

    d

---
+++ CGVProject/CGVProject/Wi/AnimationViewController.swift
@@ -0,0 +1,21 @@
+//
+//  AnimationViewController.swift
+//  CGVProject
+//
+//  Created by Wi on 18/12/2018.
+//  Copyright © 2018 Wi. All rights reserved.
+//
+
+import UIKit
+
+class AnimationViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
